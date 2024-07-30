70547bdf673fbf1f15eacbc9a800c2324938935a
Siriyakorn6 <653070310038@gmail.com>
2024-07-30 14:53:02 +0700

    สร้างคอนแทคใหม่

--- tips.sol
+++ tips.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract tips {
-     address owner;
-    constructor(){
+contract tips { //ชื่อ
+     address owner; //ที่อยู่ กับ เจ้าของ
+    constructor(){ //วิ่งครั้งแรก
         owner = msg.sender;
     }
 
