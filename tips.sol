1ddfe77beb471cf76ad25c836cc2fd1ee1cf82b6
Siriyakorn6 <653070310038@gmail.com>
2024-08-20 16:37:49 +0700

    ทำ remove , the end

--- tips.sol
+++ tips.sol
@@ -29,6 +29,7 @@ contract tips { //ชื่อ
 
     //3.2 add waitress
     function addWaitress(address payable walletAddress,string memory name) public {
+        require(msg.sender == owner, "Only the owner can call this function");
         bool waitressExist = false;
 
         if(waitress.length >=1){
@@ -47,6 +48,21 @@ contract tips { //ชื่อ
     }
 
     //4. remove user
+    function removeWaitress(address payable walletAddress) public{
+        if(waitress.length>=1){
+            for(uint i=0; i<waitress.length; i++){
+                if(waitress[i].walletAddress==walletAddress){
+                    for(uint j=i; j<waitress.length-1; j++){
+                        waitress[j]=waitress[j+1];
+                    }
+                    waitress.pop();
+                    break;
+                }
+
+            }
+            
+        }
+    }
 
     //5. view waitress
     function viewWaitress() public view returns(Waitress[] memory) {
@@ -55,5 +71,18 @@ contract tips { //ชื่อ
 
 
     //6.distribute tips
+    function distrubiteTips() public{
+        require(address(this).balance > 0, "Insufficient balance in the contract");
+        if(waitress.length>=1){
+            uint amount = address(this).balance / waitress.length;
+            for (uint i=0; i<waitress.length; i++){
+                transfer(waitress[i].walletAddress,amount);
+            }
+        }
+    }
+    //transfer money
+    function transfer(address payable walletAddress,uint amount) internal{
+        walletAddress.transfer(amount);
+    }
 
 }
